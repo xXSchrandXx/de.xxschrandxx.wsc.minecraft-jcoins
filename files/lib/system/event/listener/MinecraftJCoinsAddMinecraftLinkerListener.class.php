94d79826734febb16605f48397568876dbfd4483
xXSchrandXx <marvinschrand@yahoo.de>
2023-04-13 17:04:12 +0200

    Add DisableXsrfCheck attribute

--- files/lib/system/event/listener/MinecraftJCoinsAddMinecraftLinkerListener.class.php
+++ files/lib/system/event/listener/MinecraftJCoinsAddMinecraftLinkerListener.class.php
@@ -12,6 +12,7 @@ use wcf\system\user\jcoins\UserJCoinsStatementHandler;
  * @license  Apache License 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
  * @package  WoltLabSuite\Core\System\Event\Listener
  */
+#[\wcf\http\attribute\DisableXsrfCheck]
 class MinecraftJCoinsAddMinecraftLinkerListener implements IParameterizedEventListener
 {
     /**
--- files/lib/system/event/listener/MinecraftJCoinsRemoveMinecraftLinkerListener.class.php
+++ files/lib/system/event/listener/MinecraftJCoinsRemoveMinecraftLinkerListener.class.php
@@ -14,6 +14,7 @@ use wcf\system\user\jcoins\UserJCoinsStatementHandler;
  * @license  Apache License 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
  * @package  WoltLabSuite\Core\System\Event\Listener
  */
+#[\wcf\http\attribute\DisableXsrfCheck]
 class MinecraftJCoinsRemoveMinecraftLinkerListener implements IParameterizedEventListener
 {
     /**
